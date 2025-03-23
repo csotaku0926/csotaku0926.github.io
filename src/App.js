97cef66ca78cac0d3c07679f0dace3f4398da3b1
csotaku <ben900926.cs09@nycu.edu.tw>
2025-03-23 21:31:09 +0800

    TODO: fix broken visitors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     BrowserRouter as Router, Routes,
     Route
 } from "react-router-dom";
 import './App.css';
 import Chat from "./chat/Chat"
-import Profile from "./Profile"
+import Profile from "./profile/Profile"
 
 function App() {
-    return (
-            <div className="App">
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<Profile />} />
-                        <Route path="/chat" element={<Chat />} />
-                    </Routes>
-                </Router>
 
-            </div>
+    const [n_visitor, set_n_visitor] = useState(0);
+    
+    useEffect(() => {
+        const storedCount = sessionStorage.getItem("n_visitor");
+        const initialCount = Number(storedCount) || 0;
+        set_n_visitor(initialCount + 1);
+        sessionStorage.setItem("n_visitor", initialCount + 1);
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ul>
+                    <li>網路攻防實習</li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/chat">Chat</a></li>
+                    <li>參觀人數 {n_visitor}</li>
+                </ul>
+                <ul>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Sign Up</a></li>
+                </ul>
+            </header>
+
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Profile />} />
+                    <Route path="/chat" element={<Chat />} />
+                </Routes>
+            </Router>
+
+        </div>
     );
 }
 
---
+++ src/profile/Profile.js
@@ -0,0 +1,31 @@
+import React from "react";
+import avator from "./avator.jpg"
+import "./profile.css"
+
+const Profile = () => {
+    
+    return (
+
+        <div className="profile">
+            
+            <div>
+                <img 
+                    src={avator} 
+                    alt="my selfie" 
+                    width={'200px'} 
+                />
+                <p>國立台灣大學 電機資安所</p>
+                <p>劉沛凡</p>
+            </div>
+            <div>
+                <h2>Welcome!</h2>
+                <p>我是台大電機資安碩一生</p>
+                <p>這是我的期中專案</p>
+                <p>左邊是我的頭貼</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Profile;
